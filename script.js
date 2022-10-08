950fe34a3c9818cecff8c52cfc4bd3b66b6a991c
Jeff <jeff077872@hotmail.com>
2022-10-08 19:39:09 -0400

    second commit with some changes to javascript

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    
 });
-
+let searchBtn = $("searchBtn");
+let searchBar = $("searchBar");
 let apiKey = "6a85240e0721490a7cbc7f502516be62";
+var domain = "api.openweathermap.org";
 let city = $('#city-input');
+var path = "/geo/1.0/direct";
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+searchBtn.on("click", getApi); {
+
+    
+    
 
 
+};
+
+function getApi() {
+    
+      
+    fetch(queryURL)
+      .then(function (response) {
+        console.log("hello");
+        return response.json();
+        
+      })
+      .then(function (data) {
+        console.log(data);
+    }).catch(function(error) {
+        console.log(error);
+    });
+    
+}
 
